aaae9322bb2b2683bd5c1051a8d0b9ddfd581718
adnan-guru <im.adnan.shabbir@gmail.com>
2020-08-13 08:46:31 +0500

    start to learn javascript

---
+++ java.js
@@ -0,0 +1,24 @@
+
+/*document.write('<b><i>hello world</i></b><br>');
+document.write('hello world');*/
+
+/*var a='adnan_guru';
+var b=100;
+
+document.write(a);
+document.write(b);*/
+
+
+document.write("<table border='1px'>");
+var num = prompt("enter number for the table");
+
+  document.write("Table <br>");
+  for (j = 1; j <= 10; j++)
+  document.write(num + "*" + j + "=" + num * j + "<br>");
+document.write("</table>");
+
+
+
+
+
+
