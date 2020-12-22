1a91c777145b54f65986671f1384882610209640
adnan-guru <im.adnan.shabbir@gmail.com>
2020-12-22 21:47:09 +0500

    Learing programs

--- java.js
+++ java.js
@@ -13,7 +13,7 @@ document.write(b);*/
 var num = prompt("enter number for the table");
 
   document.write("Table <br>");
-  for (j = 1; j <= 10; j++)
+  for (j = 1; j <= 20; j++)
   document.write(num + "*" + j + "=" + num * j + "<br>");
 document.write("</table>");*/
 
@@ -23,6 +23,217 @@ document.write("</table>");*/
   document.getElementById("demo").innerHTML = Date();
   
 }*/
+
+
+
+/*var date=1;
+ var date=prompt("enter the date");
+switch (date) {
+  case 1:
+    alert("today is satureday");
+    
+    break;
+    case 2:
+      alert("today is sunday");
+      break;
+      case 3:
+        alert("today is monday");
+        
+       break;
+      case 4:
+      alert("today is tuesday");
+       break;
+      case 5:
+       alert("today is wednesday");
+       break;
+       case 6:
+        alert("today is thursday");
+        
+        break;
+        case 7:
+          alert("today is fridayday");
+          break;        
+          case 8:
+            alert("today is satureday");
+            
+            break;
+            case 9:
+              alert("today is sunayday");
+              break;    
+  default:
+    
+   alert("enter the valid date");
+}*/
+
+
+/*var a=confirm("Do you like my website");
+if(a){
+    alert("thank you");
+}else{
+ alert("sorry");
+}*/
+
+//[function]
+
+/*var a = prompt("enter your name");
+document.write(a)*/
+
+/*function hello() {
+
+  document.write('Hello Every body');
+}
+document.write("<br>")
+function adnan() {
+
+  document.write('Hello world');
+}
+hello();
+adnan();
+
+//[while loop]
+/*var a=1;
+document.write("<ul>");
+while (a <=10) {
+  document.write("<li>" +a+  ") adnan_guru </li>");
+  a= a +1;
+  document.write("</ul>");
+}*/
+
+//[do while loop]
+  /*var a = 1;
+    do{
+      document.write(a+")adnan_guru<br>")
+      a++;
+    }while(a <= 10)*/
+    
+    //[for loop]
+    /*for(a=1; a<=10; a++){
+      document.write(a+")adnan_guru <br>");
+    }*/
+      
+    //[break/continue statement]
+
+/*for (a =1 ; a <=10; a++) {
+  if(a ==3){
+    document.write("hey:" + a + "<br>");
+    continue;
+  }
+
+  document.write("number:" + a + "<br>");  
+  
+}*/
+
+/*var enterdate;
+enterdate = prompt("Enter the date to display the day");
+switch (enterdate)
+{
+    case "1":
+        alert("Today is Satureday");
+        break;
+    case "2":
+        alert(" Today is Sunday");
+        break;
+    case "3":
+        alert("Today is Monday");
+        break;
+    case "4":
+        alert("Today is Tuesday");
+        break;
+         default:
+        alert("Enter the valid date");
+}*/
+
+//even or odd num cheak
+/*for(var a = 1; a <=10; a++){
+  if(a % 2 != 0){
+    var a =prompt("enter a number")
+    document.write(a + "<br>");
+  }
+}*/
+
+//nested loop
+
+/*for(var a=1; a<=100; a=a+10){
+  for(var b=a; b<a+10; b++){
+    document.write(b+"    ")
+  }
+  document.write("<br>")
+}*/
+
+//nested loop
+
+
+/*for(a =1; a<=30; a++){
+  for(b =1 ; b<=a;  b++){
+    document.write(b +  " ")
+  }
+  document.write("<br>")
+}*/
+
+//array
+
+/*var ary = [10,20,30,40,50,60,70,80,90,100];
+var sum=0;
+document.write("<ul>")
+for(a=0; a<=9; a++){
+  document.write("<li>" +ary[a] + "</li>")
+  sum=sum+ary[a];
+}
+document.write("</ul>")
+document.write("total sum is: " +sum);*/
+
+//arry
+
+/*var a= ["Waseem","Adnan","Waqar","Talha","Zaya"]
+document.write("<h1> array</h1>")
+document.write(a + "<br><h1>Sort array</h1>");
+a.sort();
+document.write(a + "<br><h1>reverse array</h1>");
+a.reverse();
+document.write(a + "<br><h1>pop</h1>");
+a.pop();
+document.write(a + "<br><h1>push</h1>");
+a.push("usman")
+document.write(a + "<br><h1>shift</h1>");
+a.shift();
+document.write(a + "<br><h1>unshift</h1>");
+a.unshift("ferhan");
+document.write(a + "<br>");*/
+
+
+// var ary =new Array(3);
+
+
+// for(var g= 0; g < 3; g++);{
+//   ary[g]=prompt("enter the value");
+// }
+
+// document.write("<ul>")
+
+// for(var a=0; a < 3; a++){
+  
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
---
+++ shortprograms.js
@@ -0,0 +1,93 @@
+
+
+
+                                 //   program 1
+
+// 3.  write a program that inputs marks of three subjects.if the average of marks is more than 80,it displays
+// message "you are above standard, Admission Granted!". 
+
+// var input1 = parseInt(prompt("Enter Marks Subject 1 :"));
+// var input2 = parseInt(prompt("Enter Marks Subject 2 :"));
+// var input3 = parseInt(prompt("Enter Marks Subject 3 :"));
+// var result = (input1 + input2 + input3) / 3;
+
+// console.log("Subject 1 marks is :", input1);
+// console.log("Subject 2 marks is :", input2);
+// console.log("Subject 3 marks is :", input3);
+// console.log("Average Subject is :", result);
+
+// if (result >= 80) {
+//     console.log("you are above standard, Admission Granted!")
+// } else {
+//     console.log("you are fail")
+
+//}
+
+                                       //programe 2
+
+  //  1.  write a javascript program that gets temperature from user in celsius and convert it into Fahrenheit using the
+   //  formula F = 9/5 * C + 32, alert the output.
+
+            //   var celsius = prompt("Enter a celsius value: ");
+            //   var fahrenheit = (9/5 * celsius) + 32
+            //   var result = fahrenheit ;
+            //   console.log("Fahrenheight :",result)
+        
+
+
+                                        // programe 3
+
+//4. write a javascript program that inputs a number from the user and check whether it is prime number or not.
+// Assignment Requirement: 
+// var number = prompt("enter a number");
+// var num =  parseInt(number);
+
+//   if ( num % 2 == 0 ) {
+//         console.log("number is even")
+//    }
+// else{
+//       console.log("number is odd")
+//   }
+      
+
+
+                                         //   programe 4
+
+
+
+// 2.  write a javascript program that gets person height from user in inches and convert it into centimeters using
+//     the formula heighInCentimeters = 2.54 * height, console the output. 
+    
+
+var height = prompt("enter height");
+var centimeters = 2.54 * parseInt(height);
+var result = centimeters;
+console.log("Result is",result)
+
+
+
+
+
+// let num =5;
+
+// let output = 0;
+
+// for (let i = 0; i < num / 2 ; i++) {
+//   output = (output + 2);
+//   console.log("output in loop", output)
+// }
+// console.log(output)
+// if(output - num == 0){
+//   console.log("number is even")
+// }
+// else{
+//   console.log("number is odd")
+// }
+
+
+
+
+
+
+
+
