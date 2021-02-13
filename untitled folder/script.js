3e57a04dc1c29ad53249c30a9429f613466a66b4
Adnan-Guru <im.adnan.shabbir@gmail.com>
2021-02-13 23:10:53 +0500

    learn string manipulation in js

---
+++ untitled folder/script.js
@@ -0,0 +1,468 @@
+// //1. Write a js program to find maximum between two numbers.
+
+// // var num1 = parseInt(prompt("Enter number 1"))
+// // var num2 = parseInt(prompt("Enter number 2"))
+// // if (num1 < num2 ) {
+// //   console.log("Number 2 is greater than number 1");
+// // }
+
+// // //2. Write a js program to find maximum between three numbers.
+// // var number1 = 40;
+// // var number2 = 20;
+// // var number3 = 10
+// // if (number1 > number2 && number2 > number3) {
+// //   console.log("Number 1 is greater than other");
+// // }
+
+// //3. Write a js program to check whether a number is negative positive or zero.
+// //
+// //
+// //
+// //
+// //
+// //
+
+// // 4. Write a js program to check whether a number is divisible by 5 and 11 or not.
+// //
+// //
+// //
+// //
+// //
+
+// //5. Write a js program to check whether a number is even or odd.
+// //   var number = 6;
+// // if (number % 2 === 0) {
+// //   console.log("number is even");
+// // }else{
+// //   console.log("number is odd");
+// // }
+
+// //6. Write a js program to check whether a year is leap year or not.
+// //
+// //
+// //
+// //
+// //
+// //
+
+// //7. Write a js program to check whether a character is alphabet or not.
+// //var character =10;
+// //var character = "adnan";
+// // if (character === "adnan") {
+// //   console.log("character is alphabet");
+// // }else{
+// //   console.log("charater is number");
+// // }
+
+// //8. Write a js program to input any alphabet and check whether it is vowel or consonant.
+// // var alphabetNumber = "aeiou"
+// // if (alphabetNumber === "aeiou") {
+// //   console.log("These alphabet is vowel");
+// // }else{
+// //   console.log("These alphabet is consonant");
+// // }
+
+// // 9. Write a js program to input any character and check whether it is alphabet, digit or special character.
+// //var digit = "abc";
+// //var digit = 123;
+// //var digit = "$&";
+// // if (digit ==="abc") {
+
+// //   console.log("these digit is alphabet");
+// // }
+
+// // if (digit === 123) {
+// //   console.log("these are digit");
+// // }
+
+// // if (digit === "$&") {
+// //   console.log("these digit are special character");
+// // }
+
+// //10. Write a js program to check whether a character is uppercase or lowercase alphabet.
+// // var character = prompt("enter character")
+// // if (character === character.toLowerCase()) {
+// //   console.log("Character is lowercase");
+// // }else{
+// //   console.log("Character is uppercase");
+// // }
+
+// //11. Write a js program to input week number and print week day.
+
+// // var number = parseInt(prompt("enter a number"))
+// // if (number === 1) {
+// //   console.log("today is sunday")
+// // }
+// // if (number === 1) {
+// //   console.log("today is sunday")
+// // }
+// // if (number === 2) {
+// //   console.log("today is monday")
+// // }
+// // if (number === 3) {
+// //   console.log("today is tuesday")
+// // }
+// // if (number === 4) {
+// //   console.log("today is wednesday")
+// // }
+// // if (number === 5) {
+// //   console.log("today is thursday")
+// // }
+// // if (number === 6) {
+// //   console.log("today is friday")
+// // }
+// // if (number === 7) {
+// //   console.log("today is saturday")
+// // }
+// // else{
+// //   console.log("enter a valid number")
+// // }
+
+// //12. Write a js program to input month number and print number of days in that month.
+// // var monthNumber = 2;
+
+// // if (monthNumber === 1) {
+// //   console.log("month is jan and 30 days in this month")
+// // }
+// // if (monthNumber === 2) {
+// //   console.log("month is feb and 28 days in this month")
+// // }
+// // if (monthNumber === 3) {
+// //   console.log("month is march and 31 days in this month")
+// // }
+// // if (monthNumber === 4) {
+// //   console.log("month is april and 30 days in this month")
+// // }
+// // if (monthNumber === 5) {
+// //   console.log("month is may and 31 days in this month")
+// // }
+// // if (monthNumber === 6) {
+// //   console.log("month is june and 30 days in this month")
+// // }
+// // if (monthNumber === 7) {
+// //   console.log("month is july and 30 days in this month")
+// // }
+// // if (monthNumber === 8) {
+// //   console.log("month is agust and 31 days in this month")
+// // }
+// // if (monthNumber === 9) {
+// //   console.log("month is september and 30 days in this month")
+// // }
+// // if (monthNumber === 10) {
+// //   console.log("month is oct and 30 days in this month")
+// // }
+// // if (monthNumber === 11) {
+// //   console.log("month is november and 30 days in this month")
+// // }
+// // if (monthNumber === 12) {
+// //   console.log("month is december and 30 days in this month")
+// // }
+// // else{
+// // console.log("enter a valid number");
+// // }
+
+// //13. Write a js program to count total number of notes in given amount.
+// // var amount = 10000;
+// // var note = 100;
+// // var  count = amount / note;
+// // console.log("note",count);
+
+// //programe 14
+
+// // var totalAmount = prompt("Enter amount");
+
+// // if (totalAmount > 40000) {
+// //   var persantage1 = (totalAmount * 40) / 100;
+// //   var note = 500;
+// //   var count = persantage1 / note;
+// //   console.log("case in 40000+ note of 500", count);
+
+// //   var persantage2 = (totalAmount * 40) / 100;
+// //   var note2 = 1000;
+// //   var count2 = persantage2 / note2;
+// //   console.log("case in 40000+ note of 1000", count2);
+
+// //   var persantage3 = (totalAmount * 20) / 100;
+// //   var note3 = 5000;
+// //   var count3 = persantage3 / note3;
+// //   console.log("case in 40000+ note of 5000", count3);
+// // }
+
+// // if (totalAmount > 10000 && totalAmount < 40000) {
+// //   var persantage1 = (totalAmount * 50) / 100;
+// //   var note = 500;
+// //   var count = persantage1 / note;
+// //   console.log("case in 40000 note of 500", count);
+
+// //   var persantage2 = (totalAmount * 50) / 100;
+// //   var note2 = 1000;
+// //   var count2 = persantage2 / note2;
+// //   console.log("case 40000 note of 1000", count2);
+// // }
+
+// // if (totalAmount <= 10000) {
+// //   var persantage4 = (totalAmount * 80) / 100;
+// //   var note4 = 1000;
+// //   var count4 = persantage4 / note4;
+// //   console.log("case in 10000 note of 1000", count4);
+// //   var persantage5 = (totalAmount * 20) / 100;
+// //   var note5 = 500;
+// //   var count5 = persantage5 / note5;
+// //   console.log("case in 10000 note of 500", count5);
+// // }
+
+// // if (totalAmount === "") {
+// //   console.log("please enter a value");
+// // }
+
+// // if (totalAmount % 5 === 0 || totalAmount > 10000) {
+// //   var persantage7 = (totalAmount * 45) / 100;
+// //   var note7 = 500;
+// //   var count7 = persantage7 / note7;
+// //   console.log("case in 45000 note of 500", count7);
+
+// //   var persantage8 = (totalAmount * 40) / 100;
+// //   var note8 = 1000;
+// //   var count8 = persantage8 / note8;
+// //   console.log("case in 45000 note of 1000", count8);
+
+// //   var persantage9 = (totalAmount * 15) / 100;
+// //   var note9 = 5000;
+// //   var count9 = persantage9 / note9;
+// //   console.log("case in 45000 note of 5000", count9);
+// // }
+
+// ///////Arry//////
+
+// var cities = [];
+// console.log("cities", cities);
+// //add value in empty array
+// cities[0] = "gujranwala";
+// cities[1] = "ali pur chatah";
+// console.log("add cities", cities);
+// //pop delete one value last in the array
+// cities.pop();
+// console.log("pop method", cities);
+// //push add one or more value last in the array
+// cities.push("lahore", "karachi");
+// console.log("push method", cities);
+
+// //shift method remove one element biginning of the array
+// cities.shift();
+// console.log("shift", cities);
+
+// //unshift method add one or more element biginnig of the array
+// cities.unshift("islamabad", "swat", "merdan");
+// console.log("unshift method", cities);
+
+// //splice method
+// cities.splice(2, 2, "sergodha", "bhalwal", "mandi");
+// console.log("splice", cities);
+// cities.splice(0, 0, "sergodha", "bhalwal", "mandi");
+// console.log("splice B", cities);
+
+// var array = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+// console.log("slice array A", array);
+
+// var newArray = array.slice(1, 5);
+// console.log("slice array B", newArray);
+
+// var cleanestCities = ["uS", "uk", "Dubai", "arAbia", "sirya","eGypt"];
+//  console.log("upperCase");
+// for (let i = 0; i < cleanestCities.length; i++) {
+//   console.log("array loop 1 >", cleanestCities[i].toUpperCase());
+
+// }
+// console.log("lowerCase");
+// for (let i = 0; i < cleanestCities.length; i++) {
+//   console.log("array loop 2 >", cleanestCities[i].toLocaleLowerCase());
+
+// }
+
+// console.log("===========log========");
+// var cityToCheck = prompt("enter city").toLowerCase();
+// if (cityToCheck===cleanestCities[0]) {
+//   console.log("Us! it is one of the cleanest city");
+
+// }else if (cityToCheck===cleanestCities[1]) {
+//   console.log("Uk! it is one of the cleanest city")
+
+// } else if (cityToCheck===cleanestCities[2]) {
+//   console.log("dubai! it is one of the cleanest city")
+
+// }else if (cityToCheck===cleanestCities[3]) {
+//   console.log("dubai! it is one of the cleanest city")
+
+// }else if (cityToCheck===cleanestCities[4]) {
+//   console.log("dubai! it is one of the cleanest city")
+
+// }else{
+//   console.log("enter a valid city");
+// }
+
+// for (let i = 0; i <=4; i++) {
+//   if (cityToCheck===cleanestCities[i]) {
+//     console.log(" it is one of the cleanest city")
+
+// }
+// }
+
+//Function
+
+// function sum() {
+//   var num1 = parseInt(prompt("n1"));
+//   var num2 = parseInt(prompt("n2"));
+//   var result = num1+num2;
+//   console.log(result);
+// }
+// sum();
+
+// var firstName=[1,2,3,4,5,6];
+// var lastName=[7,8,9,10];
+// var fullName=[];
+// for (let i = 0; i < firstName.length; i++) {
+//     for (let j = 0; j < lastName.length; j++) {
+//     var result = fullName.push(firstName[i]+lastName[j])
+// console.log(result);
+//     }
+
+// }
+
+//18. Write a js program to calculate profit or loss.
+
+// var total = parseInt(prompt("enter amount"));
+// var spent = parseInt(prompt("enter spent amount"));
+
+// if (total < spent) {
+//   var loss = spent - total;
+//   console.log("loss >", loss);
+// } else if (total > spent) {
+//   var profit = total - spent;
+//   console.log("prfit >", profit);
+// }
+
+//19. Write a js program to input marks of five subjects Physics, Chemistry, Biology, Mathematics and Computer.
+//Calculate percentage and grade according to following:
+//Percentage >= 90% : Grade A
+//Percentage >= 80% : Grade B
+//Percentage >= 70% : Grade C
+//Percentage >= 60% : Grade D
+//Percentage >= 40% : Grade E
+//Percentage < 40% : Grade F
+
+// var subject1 = parseInt(prompt("enter physics marks"));
+// var subject2 = parseInt(prompt("enter chemistry marks"));
+// var subject3 = parseInt(prompt("enter biology marks"));
+// var subject4 = parseInt(prompt("enter math marks"));
+// var subject5 = parseInt(prompt("enter computer marks"));
+
+// var total = subject1 + subject2 + subject3 + subject4 + subject5;
+// var persantage = (total * 100) / 500;
+// if (persantage >= 90) {
+//   console.log("You are get Grade A");
+// }else if (persantage >= 80){
+//   console.log("You are get Grade B");
+
+// }else if (persantage >= 70){
+//   console.log("You are get Grade C");
+
+// }else if (persantage >= 60){
+//   console.log("You are get Grade D");
+
+// }else if (persantage >= 40){
+//   console.log("You are get Grade E");
+
+// }else if (persantage < 40){
+//   console.log("You are get Grade F");
+
+// }else{
+//   console.log("Enter only marks in number number");
+// }
+
+//20. Write a js program to input basic salary of an employee and calculate its Gross salary according to following:
+//Basic Salary <= 10000 : HRA = 20%, DA = 80%
+//Basic Salary <= 20000 : HRA = 25%, DA = 90%
+//Basic Salary > 20000 : HRA = 30%, DA = 95%//
+
+// var salary = parseInt(prompt("Enter your salary"));
+
+// if (salary <= 10000) {
+//   console.log("HRA = 20%, DA = 80%");
+// } else if (salary <= 20000) {
+//   console.log("HRA = 25%, DA = 90%");
+// } else if (salary > 20000) {
+//   console.log("HRA = 30%, DA = 95%");
+// } else {
+//   console.log("Enter only salary in number");
+// }
+
+//21. Write a js program to input electricity unit charges and calculate total electricity bill according to the given condition:
+//For first 50 units Rs. 0.50/unit
+//For next 100 units Rs. 0.75/unit
+//For next above 100 units Rs. 1.20/unit
+//For unit above 250 Rs. 1.50/unit
+//An additional surcharge of 20% is added to the bill
+
+// var unit = parseInt(prompt("enter your electricity unit"));
+
+// if (unit <= 50) {
+//   var bill = unit * 0.5;
+//   console.log(`Your electricity bill is > ${bill} rupees`);
+// } else if (unit <= 100) {
+//   var bill = unit * 0.75;
+//   console.log(`Your electricity bill is > ${bill} rupees`);
+// } else if (unit > 100 && unit <= 250) {
+//   var bill = unit * 1.2;
+//   console.log(`Your electricity bill is > ${bill} rupees`);
+// } else if (unit > 250) {
+//   var bill = unit * 1.2;
+//   console.log("bill", bill);
+//   var charge = (unit * 20) / 100;
+//   var totalBill = bill + charge;
+
+//   console.log(`Your electricity bill is > ${totalBill} rupees`);
+// }
+
+//10. Write a js program to check whether a character is uppercase or lowercase alphabet.
+
+// var character = prompt("enter a character");
+// var  value = character.toLowerCase();
+// if (character === value) {
+//   alert("character is lower case");
+// } else  {
+//   alert("character is uppercase case");
+// }
+
+// var cityToCheck = prompt("Enter city");
+// //cityToCheck = cityToCheck.toLowerCase();
+// var cleanCities = ["United state", "uk", "Dubai", "paris", "hjkhkj", "jhgkhj"];
+// for (let i = 0; i < 4; i++) {
+//   if (cityToCheck === cleanCities[i]) {
+//     alert("this is clean city");
+//     console.log(cleanCities[i]);
+//   } else{
+//     console.log("enter valid city");
+//   }
+// }
+
+// var addCharacter = prompt("Enter name");
+// var firstChar = addCharacter.slice(0,1);
+// var cutCh= firstChar.toUpperCase(),
+//  someChar = addCharacter.slice(1,addCharacter.length);
+// var newCh = cutCh + someChar
+// console.log("capital methothod >",newCh);
+
+// var character = "adnan";
+// var copyCh = character.slice(0, 1);
+// var upperCase = copyCh.toUpperCase();
+// var otherCh = character.slice(1, 5);
+// var newCh = upperCase + otherCh;
+// console.log("new ch", newCh);
+
+
+// var month = prompt("enter month");
+// var charMonth = month.length;
+// if (charMonth > 3) {
+//   var newCh = month.slice(0,3)
+//   console.log("month >",newCh);
+// }
